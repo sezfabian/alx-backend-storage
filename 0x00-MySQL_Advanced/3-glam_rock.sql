ddab24988bccb4fbd91ee7b7d4f7fb692875edbb
sezfabian <cheruiyotfabian@gmail.com>
2023-08-10 01:11:53 +0300

    IFNULL

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- lists all bands with Glam rock as their main style
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
