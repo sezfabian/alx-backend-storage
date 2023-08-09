b4689478c7828fcdf8f9844962da9b5a1249fdb4
sezfabian <cheruiyotfabian@gmail.com>
2023-08-10 02:29:07 +0300

    triggers

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,5 +1,4 @@
  -- creates a trigger that decreases the quantity of an item after adding a new order
- DROP TRIGGER IF EXISTS decrease_quantity;
  CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
  FOR EACH ROW
  UPDATE items
