c6794007f23a2b7ed9015c348a2b1e8717542f68
sezfabian <cheruiyotfabian@gmail.com>
2023-08-09 23:59:52 +0300

    unique

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- script that creates a table users
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	n<t_k€>krame VARCHAR(255)
+);
+
